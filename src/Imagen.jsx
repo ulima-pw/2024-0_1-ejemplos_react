bc2fcd0e0e67bf589e1a916bd12a4e0628a8845d
ProfL3404 <ProfL3404@lab.aca>
2024-01-22 12:01:48 -0500

    Listado de imagenes terminado

--- src/Imagen.jsx
+++ src/Imagen.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
+
+
 const Imagen = (props) => {
+    // Variable de estado
+    const [color, setColor] = useState("#FFFFFF")
+
     const estiloCard = {
         width : "300px",
-        marginBottom : "20px"
+        marginBottom : "20px",
+        backgroundColor : color
     }
-    return <div className="card" style={ estiloCard }>
+
+    const cardOnClick = () => {
+        // Modifique el color del card
+        if (color == "#FFFFFF") setColor("#FF10FF")
+        else setColor("#FFFFFF") 
+        // Modifica la variable de estado (regenera componente)
+    }
+
+    return <div 
+                className="card" 
+                style={ estiloCard }
+                onClick={ cardOnClick }>
         <img 
             src={ props.url } className="card-img-top" />
         <div className="card-body">
